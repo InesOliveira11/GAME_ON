734d2960a26fe2685d0d6c4d07c2cdc4861fd2d3
Yuliia Topalova <yuliia.topalova@gmail.com>
2021-11-17 15:09:30 +0000

    fixing bugs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,7 +11,8 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    if @game.save
+    @game.user = current_user
+    if @game.save!
       redirect_to games_path
     else
       render :new
